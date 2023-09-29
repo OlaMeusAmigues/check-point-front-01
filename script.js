3e120d55fbaa4429578ac6f34156837f8325fd9c
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-29 08:56:32 -0300

    pronto

---
+++ script.js
@@ -0,0 +1,22 @@
+let nome = "Gustavo";
+let idade = 20;
+let status = true;
+let valorProduto = 10.5;
+let listaTimes = ["Corinthians", "Grêmio", "Palmeiras"];
+
+console.log(nome);
+console.log(idade);
+console.log(status);
+console.log(valorProduto);
+console.log(listaTimes);
+
+let n1 = 4;
+let n2 = 2;
+let n3 = -6;
+let n4 = 6;
+let n5 = 9;
+
+console.log(n1,"+",n2,"+(",n3,")+",n4,"+",n5," = ",n1+n2+n3+n4+n5);
+console.log(n1,"-",n2,"-(",n3,")-",n4,"-",n5," = ",n1-n2-n3-n4-n5);
+console.log(n1,"x",n2,"x(",n3,")x",n4,"x",n5," = ",n1*n2*n3*n4*n5);
+console.log(n1,"/",n2,"/(",n3,")/",n4,"/",n5," = ",n1/n2/n3/n4/n5);
